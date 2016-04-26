86c7fb656525803799ba3d3b9af5f23b454c871f
Phil Weir <phil.weir@numa.ie>
2016-04-26 13:27:14 +0100

    update style in tests

--- tests/test_launch_app.py
+++ tests/test_launch_app.py
@@ -2,34 +2,18 @@ import pytest
 import asyncio.coroutines
 import asyncio
 from unittest.mock import MagicMock
-import time
 import uuid
-import traceback
-import pdb
-import lxml.etree
-import builtins
-from unittest.mock import MagicMock, patch, mock_open
 
 from dockerlaunch.app import DockerLaunchApp
 
-import docker
-import tempfile
-import requests
-import threading
-import os
-import signal
-import traceback
-import re
 
-
-known_guid   = str(uuid.uuid4()).upper()
+known_guid = str(uuid.uuid4()).upper()
 unknown_guid = str(uuid.uuid4()).upper()
 
 
 def magic_coro():
     mock = MagicMock()
     return mock, asyncio.coroutine(mock)
-
 
 
 @asyncio.coroutine
@@ -39,54 +23,42 @@ def wait():
     yield from asyncio.gather(*relevant_tasks)
 
 
-
 @pytest.fixture(scope="function")
-def launchapp( monkeypatch ):
-    docker_settings     = MagicMock()
-    socket_location     = MagicMock()
-    pidfile_path        = MagicMock()
-    logger              = MagicMock()
-    docker_socket       = MagicMock()
-    shutdown_timeout    = MagicMock()
-    launchapp = DockerLaunchApp ( docker_settings, socket_location,
-                 pidfile_path, logger, pidfile_timeout=5,
-                 stdout_path='/dev/tty', stderr_path='/dev/tty',
-                 stdin_path='/dev/null')
+def launchapp(monkeypatch):
+    docker_settings = MagicMock()
+    socket_location = MagicMock()
+    pidfile_path = MagicMock()
+    logger = MagicMock()
+    launchapp = DockerLaunchApp(
+        docker_settings, socket_location,
+        pidfile_path, logger, pidfile_timeout=5,
+        stdout_path='/dev/tty', stderr_path='/dev/tty',
+        stdin_path='/dev/null')
     launchapp.launchapp = MagicMock()
-    return launchapp 
+    return launchapp
 
 
-
-    ###############################
-    ######### LEGACY.PY ###########    
-    ###############################   
-
-
-
-def test_terminate  ( monkeypatch , launchapp ) :
-    random_signo        = MagicMock()
-    random_stackframe   = MagicMock()
-    launchapp._logger   = MagicMock()
-    launchapp._server   = MagicMock()
-    monkeypatch.setattr( 'os.access'  , lambda p1 , p2 : True )
-    monkeypatch.setattr( 'os.remove'  , lambda p1      : True )
-    launchapp.terminate ( random_signo , random_stackframe )
+def test_terminate(monkeypatch, launchapp):
+    random_signo = MagicMock()
+    random_stackframe = MagicMock()
+    launchapp._logger = MagicMock()
+    launchapp._server = MagicMock()
+    monkeypatch.setattr('os.access', lambda p1, p2: True)
+    monkeypatch.setattr('os.remove', lambda p1: True)
+    launchapp.terminate(random_signo, random_stackframe)
     launchapp._logger.info.assert_called_with("Server shutdown")
     launchapp._server.shutdown.assert_called_with()
 
 
-
-def test_run  ( monkeypatch , launchapp ) :
-    launchapp._logger           = MagicMock()
-    launchapp._server           = MagicMock()
-    launchapp.socket_location   = MagicMock()    
-    panos12                     = MagicMock()
-    monkeypatch.setattr( 'dockerlaunch.handler.ThreadedUnixRequestHandler'  , panos12  )
-    monkeypatch.setattr( 'dockerlaunch.server.ThreadedUnixServer'  , lambda p1 , panos7 : panos7 ( 'p1' , 'p2' , 'panosargs3' , panos = 'p4' ) )
-    monkeypatch.setattr( 'signal.signal'  , lambda p1 , p2 : True )
-    launchapp.run (  )
-    launchapp._logger.info.assert_called_with ("Starting server")
-
-
-
-    
+def test_run(monkeypatch, launchapp):
+    launchapp._logger = MagicMock()
+    launchapp._server = MagicMock()
+    launchapp.socket_location = MagicMock()
+    tst12 = MagicMock()
+    monkeypatch.setattr('dockerlaunch.handler.ThreadedUnixRequestHandler',
+                        tst12)
+    monkeypatch.setattr('dockerlaunch.server.ThreadedUnixServer',
+                        lambda p1, tst7: tst7('p1', 'p2', 'tstarg3', tst='p4'))
+    monkeypatch.setattr('signal.signal', lambda p1, p2: True)
+    launchapp.run()
+    launchapp._logger.info.assert_called_with("Starting server")
--- tests/test_launch_handler.py
+++ tests/test_launch_handler.py
@@ -2,25 +2,13 @@ import pytest
 import asyncio.coroutines
 import asyncio
 from unittest.mock import MagicMock
-import time
 import uuid
-import traceback
-import pdb
-import lxml.etree
-import builtins
-from unittest.mock import MagicMock, patch, mock_open , Mock
+from unittest.mock import patch, Mock
 
 from dockerlaunch.handler import ThreadedUnixRequestHandler
 
-import docker
-import tempfile
-import requests
-import os
-import traceback
-import re
 
-
-known_guid   = str(uuid.uuid4()).upper()
+known_guid = str(uuid.uuid4()).upper()
 unknown_guid = str(uuid.uuid4()).upper()
 
 
@@ -37,65 +25,43 @@ def wait():
 
 
 @pytest.fixture(scope="function")
-def handler( monkeypatch ):
-    docker_settings     = MagicMock()
-    logger              = MagicMock()
-    docker_socket       = MagicMock()
-    shutdown_timeout    = MagicMock()
-    arg1                = MagicMock()
-    arg2                = MagicMock()
-    arg3                = MagicMock()
+def handler(monkeypatch):
+    docker_settings = MagicMock()
+    logger = MagicMock()
+    arg1 = MagicMock()
+    arg2 = MagicMock()
+    arg3 = MagicMock()
 
-    monkeypatch.setattr( 'docker.Client'    , lambda version , base_url : 2123456 )
-    monkeypatch.setattr( 'dockerlaunch.layer.DockerLayer' , lambda logger=None : 'p2' )
-    
+    monkeypatch.setattr('docker.Client', lambda version, base_url: 2123456)
+    monkeypatch.setattr('dockerlaunch.layer.DockerLayer',
+                        lambda logger=None: 'p2')
+
     class turh_parent(Mock):
         _logger = 1
-                
-    patcher = patch.object ( ThreadedUnixRequestHandler , "__bases__" , ( turh_parent , ) )
-    with patcher: 
+
+    patcher = patch.object(ThreadedUnixRequestHandler, "__bases__",
+                           (turh_parent,))
+    with patcher:
         patcher.is_local = True
-        handler = ThreadedUnixRequestHandler ( docker_settings, logger , arg1 , arg2 , arg3 )
+        handler = ThreadedUnixRequestHandler(
+            docker_settings, logger, arg1, arg2, arg3
+        )
 
-    return handler 
+    return handler
 
 
+def test_configure(monkeypatch, handler):
+    random_docker_settings = MagicMock()
+    handler.logger = MagicMock()
+    handler._docker_layer = MagicMock()
+    random_docker_settings = {'logger': 'logger1'}
+    handler._configure(random_docker_settings)
+    # dockerlaunch.layer.DockerLayer.assert_called_with ( arg1 , arg2 , arg3 )
 
-    ###############################
-    ######### LEGACY.PY ###########    
-    ###############################   
- 
-    
- 
-def test_configure ( monkeypatch , handler ):
-    random_docker_settings  = MagicMock()
-    handler.logger          = MagicMock()
-    handler._docker_layer   = MagicMock()    
-    random_docker_settings = { 'logger' : 'logger1' }
-    handler._configure ( random_docker_settings )
-    #dockerlaunch.layer.DockerLayer.assert_called_with ( arg1 , arg2 , arg3 )
-    
-    
 
-#def test_handle ( monkeypatch , handler ) :
-    #handler.connection = MagicMock()
-    #monkeypatch.setattr ( 'select.select', lambda p1 , p2 , p3 : ( True  , True , True ) )
-    #data007 = MagicMock()
-    #data007.index.return_value = -7
-    #handler.connection.recv.return_value = data007
-    #handler.handle()
-    
-    
-    
-def test_process_message1  ( monkeypatch , handler ) :
-    random_message          = MagicMock()
-    random_arguments        = MagicMock()
-    random_message          =   "START"
-    random_arguments        = { 'image' : 'pic1' , 'update socket' : True }
-    handler._docker_layer   = MagicMock()
-    handler._docker_layer.try_launch.return_value = ( True , "LOGS" )
-    result = handler._process_message ( random_message , random_arguments )
-    
-    
-    
-    
+def test_process_message1(monkeypatch, handler):
+    random_message = "START"
+    random_arguments = {'image': 'pic1', 'update socket': True}
+    handler._docker_layer = MagicMock()
+    handler._docker_layer.try_launch.return_value = (True, "LOGS")
+    handler._process_message(random_message, random_arguments)
